67539caf047d9e9cea44eb157cbf33930b9264b9
Socheath Horl <horl.socheath@gmail.com>
2023-11-11 11:25:04 +0700

    crop image

--- img_proc.py
+++ img_proc.py
@@ -6,5 +6,11 @@ img = Image.open('george-side.jpg')
 # print image info
 print(img.size)
 print(img.format)
+
+# crop image
+area = (100, 100, 500, 500)
+crop_img = img.crop(area)
+
 # show image
 img.show()
+crop_img.show()
